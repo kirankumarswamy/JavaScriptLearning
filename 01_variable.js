a5dffb650eefd1659bd820b209692b3a28f657e9
kirankumarswamy <swamykirankumar38@gmail.com>
2023-07-19 09:51:59 +0530

    javascript basics
    javascript basics

---
+++ 01_variable.js
@@ -0,0 +1,11 @@
+
+var myName; // this is my variable declaration
+myName = "Kirankumar"; // variable Assignment ot initialization
+console.log(myName);
+
+
+var city = "Bidar"; // variable declaration & initialization
+console.log(city);
+
+var pin;
+console.log(pin);
---
+++ 02_dataType.js
@@ -0,0 +1,22 @@
+
+
+var firstName = "Kirankumar";
+console.log(typeof firstName);
+
+ var age = 24;
+ console.log(typeof age);
+
+var isMarried = false;
+console.log(typeof isMarried);
+
+var city = "Bidar";
+console.log(typeof city);
+
+city = 50000
+ console.log(typeof city);
+
+var marks;
+console.log( marks);
+console.log( typeof marks);
+
+ 
