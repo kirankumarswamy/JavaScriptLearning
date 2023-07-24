7fe10e813623b943ea39041312c5757c534744a4
kirankumarswamy <swamykirankumar38@gmail.com>
2023-07-24 21:02:05 +0530

    basic Operators
    basic Operators

---
+++ 05_operators.js
@@ -0,0 +1,42 @@
+// + Addition
+// - Subtraction
+// * Multiplication
+console.log(`-------- Exponential ------------`);
+var num1 = 10;
+var num2 = 3;
+var result = num1 ** num2;
+console.log(`Result is : ${result}`);
+
+console.log(`-------- Division ------------`);
+var result = num1 / num2;
+console.log(`Result is : ${result}`);
+
+console.log(`-------- Modulus ------------`);
+var result = num1 % num2;
+console.log(`Result is : ${result}`);
+console.log(` ${15%3}`);
+console.log(`${12%2}`);
+
+console.log(`--------- Assignment Operators -------------`);
+console.log(`---------  Compound addition -------------`);
+var num1 = 10;
+num1+=5; // num1 + 5;  num1 = 15
+console.log(`Result is : ${num1}`);
+
+console.log(`---------  Compound Subtraction -------------`);
+num1-=5;
+console.log(`Result is : ${num1}`);
+
+console.log(`---------  Compound Multiplication -------------`);
+num1*=5;
+console.log(`Result is : ${num1}`);
+
+console.log(`""""""""""""""""""""""""`);
+var num2 = 20;
+num2 =+5;
+console.log(num2);
+
+console.log(`""""""""""""""""""""""""`);
+var num2 = 20;
+num2 =-10;
+console.log(num2);
