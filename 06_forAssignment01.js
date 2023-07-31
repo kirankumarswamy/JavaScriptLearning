fe3509da0d1b8f8de3edffa7d6a389e349b4d46a
kirankumarswamy <swamykirankumar38@gmail.com>
2023-07-31 13:17:34 +0530

    using while loop,assignment  of for loop
    using while loop , assignment  of for loop

--- 06_forAssignment01.js
+++ 06_forAssignment01.js
@@ -59,3 +59,68 @@ for (let index = 100; index >=10; index= index-10) {
 }
 console.log(`${result}`);
 console.log(`------------------------------------`);
+
+
+
+console.log(`Using while loop`);
+
+
+console.log(`=================Assignment-2=================`);
+console.log(`1)Numbers from 5 to 15 by incrementing 1`);
+ var i=5;
+ while (i<=15) {
+    console.log(i);
+    i++;
+ }
+
+ console.log(`------------------------------------`);
+console.log(`2)Numbers from 50 to 40 by decrementing 1`);
+ var i = 50;
+ while (i>=40) {
+    console.log(i);
+    i--;
+ }
+
+ console.log(`------------------------------------`);
+console.log(`3)First 15 odd numbers`);
+var i = 1;
+while (i<30) {
+   console.log(i);
+   i+=2 ;
+}
+
+
+console.log(`------------------------------------`);
+console.log(`4)First 10 even numbers`);
+ var i =0;
+ while (i<20) {
+    console.log(i);
+    i+=2;
+ }
+
+
+ console.log(`------------------------------------`);
+console.log(`5)Table of 5`);
+ var i =5;
+ while (i<=50) {
+    console.log(i);
+    i+=5;
+ }
+
+
+console.log(`------------------------------------`);
+console.log(`6)Table of 10`);
+ var i =10;
+ while (i<=100) {
+    console.log(i);
+    i+=10;
+ }
+
+
+ console.log(`------------------------------------`);
+console.log(`7)Table of 10 in reverse`);
+ var i =100;
+ while (i>=10) {
+    console.log(i);
+    i-=10;
+ }
