4cf10032457165aa918a77e0412eb2553f3aa566
kirankumarswamy <swamykirankumar38@gmail.com>
2023-08-07 17:49:28 +0530

    date in js
    Javascript Date

---
+++ 09_date.js
@@ -0,0 +1,9 @@
+
+
+let date = new Date ();
+console.log(date);
+console.log(date.getDate());
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleTimeString());
+console.log(date.getFullYear());
+console.log(date.toJSON());
