abbe479854a6182631f8b80830f38c6fb5012023
kirankumarswamy <swamykirankumar38@gmail.com>
2023-08-08 13:28:53 +0530

    Object Cloning assignment  A and B

---
+++ 10_objectCloneAssignA.js
@@ -0,0 +1,74 @@
+
+
+console.log(`--------------------------=========================Assignment-1=======================------------------------------------`);
+console.log(`------------------------------------------------Step-1----------------------------------------------------------`);
+let personalDetails = {
+    fullName : "Kirankumar Swamy",
+    age : 24,
+    gender: "Male",
+    garduation : "Bachelor of Engineering",
+    phoneNUmber : 8095643752,
+    emailId : "kirankumarswamy1411@gmail.com",
+    address : "Bidar Karnataka"
+
+}
+console.log(personalDetails);
+
+console.log(`------------------------------------------------Step-2----------------------------------------------------------`);
+
+let collegeDetails = {
+    collegeName : "GuruNanak Dev Engineering college",
+    location : "Bidar,Karnataka",
+    fieldOfStudy : "Civil Engineering",
+    passOutYear : 2021,
+    securedMarks : "67%"
+}
+console.log(collegeDetails);
+
+console.log(`------------------------------------------------Step-3----------------------------------------------------------`);
+let myDetailsMerged = Object.assign({},personalDetails,collegeDetails)
+console.table(myDetailsMerged);
+
+console.log(`------------------------------------------------Step-4----------------------------------------------------------`);
+console.log(` Creating aaray of my Friends name`);
+ let friendsname = ['Guru','Akash','Vaibhav','Sachin']
+ console.log(` Friends Name : ${friendsname}`);
+ Object.freeze(friendsname)
+//  friendsname.push("amar")
+//  console.log(` Friends Name : ${friendsname}`);
+
+console.log(`------------------------------------------------Step-5----------------------------------------------------------`);
+console.log(`Using for loop, Loging Frinds Name on Console`);
+for (const friends of friendsname) {
+    console.log(`Friend name : ${friends}`);
+    
+}
+
+console.log(`------------------------------------------------Step-6----------------------------------------------------------`);
+//console.log(`Using Spread operator ,string reverse`);
+// console.log(`Given string is : "Technology"`);
+// const str = "Technology"
+// const reverseStr = [...str].reverse().join("");
+// console.log(reverseStr);
+
+                  // OR //
+ console.log(`Using  for loop , reversing string`);
+console.log(`Given string is : "Technology"`);
+ const string = "Technology"
+ let reversedString  = "";
+ for (let i = string.length-1; i >= 0; i--) {
+  reversedString  += string[i]
+ }
+ console.log(`Reversed string is : "${reversedString}"`);
+
+                  // OR //
+// console.log(`Using while Loop and substring() method`);
+// console.log(`Given string is : "Technology"`);
+// let str = "Technology"
+// let strReversed = " ";
+// let i = str.length-1;
+// while(i>=0){
+//     strReversed += str.substring(i,i+1);
+//     i--
+// }
+// console.log(`Reversed string is : "${strReversed}"`);
---
+++ 10_objectCloneAssignB.js
@@ -0,0 +1,67 @@
+
+console.log(`======================================Assignment=2=========================================`);
+console.log(`-----------------------------------------------Step-1&2----------------------------------------------------`);
+
+const bankSbi = {
+    bankName : "SBI Bank",
+    location  : "Bidar",
+    accountNo : "39449678902",
+    ifsc : "SBIN0002035",
+}
+console.log(`   Before Cloning Objects are `);
+console.log(bankSbi);
+
+
+const bankLocation = {
+    street : "near Deputy Commissioner's Office, Sgivaji circle Bidar ",
+    city : "Bidar",
+    pincode : 585401
+}
+
+console.log(bankLocation);
+
+console.log(`-----------------------------------------------Step-3----------------------------------------------------`);
+
+console.log(`   Using Object.assign() For cloning Bank and Location`);
+
+let cloneSbi = Object.assign({},bankSbi)
+// cloneSbi.location = "Bengaluru"
+console.log(cloneSbi);
+
+let cloneLocation = Object.assign({},bankLocation);
+// cloneLocation.city = "Mysore";
+console.log(cloneLocation);
+
+console.log(` `);
+
+console.log(`   Using spread operator for Cloning Bank and Location`);
+let clonedSbiBank = {...bankSbi}
+console.log(clonedSbiBank);
+
+let clonedLocation = {...bankLocation}
+console.log(clonedLocation);
+
+console.log(`--------------------------------Step-4---------------------------------`);
+console.log(`    Rate Of interest properties`);
+const rateOfInterest = {
+    homeLoanInterest : "4.8%",
+    personalLoanInterest : "5.2%",
+    duelInterest : "5.0%"
+}
+console.log(rateOfInterest);
+
+console.log(`-------------------------------------step-5----------------------------------------`);
+console.log(`    Merging step1,step2 and step4 objects into new object sbitDetails`);
+
+let mergedObjects = Object.assign({},bankSbi,bankLocation,rateOfInterest)
+console.table(mergedObjects);
+
+console.log(`-------------------------------------step-6----------------------------------------`);
+console.log(`-----Traversing Merged object Using for in loop-----`);
+
+for (const value in mergedObjects) {
+    if (Object.hasOwnProperty.call(mergedObjects, value)) {
+        const element = mergedObjects [value];
+        console.log(`Bank details :${value} : ${element}`);
+    }
+}
