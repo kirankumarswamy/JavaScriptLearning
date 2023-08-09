12e34ea42e4d9cfcf6235810ee5177c236f9ada4
kirankumarswamy <swamykirankumar38@gmail.com>
2023-08-09 19:48:09 +0530

    practiced logging Highest and lowest number from array
    practiced logging Highest and lowest number from array

---
+++ highestAndlowestNumber.js
@@ -0,0 +1,9 @@
+
+
+
+const numArray = [10,20,17,25,66,44]
+let highest = Math.max(...numArray)
+let lowest = Math.min(...numArray)
+console.log(`Highest number : ${highest}`);
+console.log(`Lowest number : ${lowest}`);
+
