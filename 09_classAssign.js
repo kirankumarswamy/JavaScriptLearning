fe14ce8456acbc5de798217bd8a0fc2e207249e8
kirankumarswamy <swamykirankumar38@gmail.com>
2023-08-10 12:34:55 +0530

    made changes in 09_classAssign.js
    made changes in 09_classAssign.js, removed prime number code from this file and added in Primenumber file

--- 09_classAssign.js
+++ 09_classAssign.js
@@ -93,34 +93,3 @@ console.log(`--------------------------------`);
 traverseObject(college3)
 console.log(`--------------------------------`);
 traverseObject(college4)
-
-
-console.log(`===================================================step-4=====================================================`);
-
-console.log(`//Program to check if entered number is prime or not//`)   
-
-function isprime (n){
-    if (n===1) {
-        return `You entered 1 & is not prime number`
-    }
-    else if (n===2){
-        return `Is prime`
-    }
-    else {
-        for (let x = 2; x < n; x++) {
-            if (n%x===0) {
-                return `Entered number is ${n} & is not prime number`
-            }
-            
-        }
-        return`Entered number is ${n} & is prime number `;
-         
-    }
-}
-console.log(isprime(11));
-console.log(isprime(12));
-console.log(isprime(100));
-console.log(isprime(29));
-console.log(isprime(179));
-console.log(isprime(null));
-console.log(isprime(1));
